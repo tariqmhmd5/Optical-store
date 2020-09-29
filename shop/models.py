b94115db1f04062f58968271a2507773f040046e
tariqmhmd5 <tariqmhmd25@gmail.com>
2020-09-30 01:48:59 +0530

    working cart

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE ,null=True,blank=True)
+    name = models.CharField(max_length=100,null=True)
+    email = models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return self.name
+    
 
 class Product(models.Model):
-    product_id = models.AutoField
     product_name = models.CharField(max_length=50)
     desc = models.CharField(max_length=300)
     category = models.CharField(max_length=50,default="",choices=(('Men Eyewear','Men Eyewear'),('Women Eyewear','Women Eyewear')))
@@ -11,4 +20,50 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False,null=True,blank=False)
+    transaction_id = models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
+    order = models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
+    quantity = models.IntegerField(default=0,null=True,blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)   
+    order = models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    address = models.CharField(max_length=100,null=True)
+    city = models.CharField(max_length=100,null=True)
+    state = models.CharField(max_length=100,null=True)
+    zipcode = models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return self.address
+    
     
--- shop/views.py
+++ shop/views.py
@@ -1,19 +1,43 @@
-from django.shortcuts import render
-from .models import Product
-from math import ceil
+from django.shortcuts import render, HttpResponse
+from . models import *
 
 def products(request):
     products = Product.objects.all()
-    n= len(products)
     params = {'product':products}
     return(render(request,'shop/products.html',params))
 
 
 def cart(request):
-    return(render(request,'shop/cart.html'))
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0,"get_cart_items":0}
+        #return(HttpResponse("Nothing in Cart Login to Continue"))
+    
+    context = {
+        'items':items,
+        'order':order
+    }
+    return(render(request,'shop/cart.html',context))
 
 def checkout(request):
-    return(render(request,'shop/checkout.html'))
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0,"get_cart_items":0}
+        #return(HttpResponse("Nothing in Cart Login to Continue"))
+    
+    context = {
+        'items':items,
+        'order':order
+    }
+    return(render(request,'shop/checkout.html',context))
 
 def view_product(request,id):
     prod = Product.objects.filter(id=id)
